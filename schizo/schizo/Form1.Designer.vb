91b4c029062513c9eb61721d1d258d0d364a6cc9
Stav ros <hackerone1@web.de>
2018-04-25 18:46:03 +0200

    basic

---
+++ schizo/schizo/Form1.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.InviteBt = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.WebBrowser1.Location = New System.Drawing.Point(0, 98)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(1111, 515)
+        Me.WebBrowser1.TabIndex = 0
+        Me.WebBrowser1.Url = New System.Uri("https://www.facebook.com/events/225323714695725/", System.UriKind.Absolute)
+        '
+        'Timer1
+        '
+        '
+        'InviteBt
+        '
+        Me.InviteBt.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.InviteBt.Location = New System.Drawing.Point(12, 4)
+        Me.InviteBt.Name = "InviteBt"
+        Me.InviteBt.Size = New System.Drawing.Size(85, 29)
+        Me.InviteBt.TabIndex = 1
+        Me.InviteBt.Text = "Invite"
+        Me.InviteBt.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.ForeColor = System.Drawing.Color.Red
+        Me.TextBox1.Location = New System.Drawing.Point(103, 4)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(124, 29)
+        Me.TextBox1.TabIndex = 2
+        Me.TextBox1.Text = "NOT READY"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(261, 9)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(261, 57)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(405, 57)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 5
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(430, 10)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Button2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1111, 613)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.InviteBt)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Name = "Form1"
+        Me.Text = "Schizo"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents InviteBt As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button2 As Button
+End Class
---
+++ schizo/schizo/Form1.vb
@@ -0,0 +1,151 @@
+﻿Imports mshtml
+Public Class Form1
+    Dim pswaHeight As Object
+    Dim pswaWidth As Object
+    Dim pswaHeightInt As Integer
+    Dim pswaWidthInt As Integer
+
+    Private Enum Exec
+        OLECMDID_OPTICAL_ZOOM = 63
+    End Enum
+
+    Private Enum execOpt
+        OLECMDEXECOPT_DODEFAULT = 0
+        OLECMDEXECOPT_PROMPTUSER = 1
+        OLECMDEXECOPT_DONTPROMPTUSER = 2
+        OLECMDEXECOPT_SHOWHELP = 3
+    End Enum
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        pswaHeight = Screen.PrimaryScreen.WorkingArea.Height
+        pswaWidth = Screen.PrimaryScreen.WorkingArea.Width
+        pswaHeightInt = CInt(pswaHeight)
+        pswaWidthInt = CInt(pswaWidth)
+    End Sub
+
+    Private Sub login_facebook()
+        WebBrowser1.Document.GetElementById("Email").SetAttribute("value", TextBox2.Text)
+        WebBrowser1.Document.GetElementById("pass").SetAttribute("value", TextBox3.Text)
+        WebBrowser1.Document.GetElementById("loginbutton").InvokeMember("click")
+    End Sub
+
+    Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+
+        TextBox1.Text = "READY"
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+
+        If WebBrowser1.Width > Math.Round(pswaWidthInt / 5.6) And WebBrowser1.Width < Math.Round(pswaWidthInt / 2.97) Or WebBrowser1.Height > Math.Round(pswaHeightInt / 3.0) And WebBrowser1.Height < Math.Round(pswaHeightInt / 2.4) Then
+            Try
+                Dim Res As Object = Nothing
+                Dim MyWeb As Object
+                MyWeb = Me.WebBrowser1.ActiveXInstance
+                MyWeb.ExecWB(Exec.OLECMDID_OPTICAL_ZOOM, execOpt.OLECMDEXECOPT_PROMPTUSER, 40, IntPtr.Zero)
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End If
+
+        If WebBrowser1.Width > Math.Round(pswaWidthInt / 2.97) And WebBrowser1.Width < Math.Round(pswaWidthInt / 2.58) Or WebBrowser1.Height > Math.Round(pswaHeightInt / 2.4) And WebBrowser1.Height < Math.Round(pswaHeightInt / 2.13) Then
+            Try
+                Dim Res As Object = Nothing
+                Dim MyWeb As Object
+                MyWeb = Me.WebBrowser1.ActiveXInstance
+                MyWeb.ExecWB(Exec.OLECMDID_OPTICAL_ZOOM, execOpt.OLECMDEXECOPT_PROMPTUSER, 50, IntPtr.Zero)
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End If
+
+        If WebBrowser1.Width > Math.Round(pswaWidthInt / 2.58) And WebBrowser1.Width < Math.Round(pswaWidthInt / 2.23) Or WebBrowser1.Height > Math.Round(pswaHeightInt / 2.13) And WebBrowser1.Height < Math.Round(pswaHeightInt / 1.85) Then
+            Try
+                Dim Res As Object = Nothing
+                Dim MyWeb As Object
+                MyWeb = Me.WebBrowser1.ActiveXInstance
+                MyWeb.ExecWB(Exec.OLECMDID_OPTICAL_ZOOM, execOpt.OLECMDEXECOPT_PROMPTUSER, 60, IntPtr.Zero)
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End If
+
+        If WebBrowser1.Width > Math.Round(pswaWidthInt / 2.23) And WebBrowser1.Width < Math.Round(pswaWidthInt / 1.89) Or WebBrowser1.Height > Math.Round(pswaHeightInt / 1.85) And WebBrowser1.Height < Math.Round(pswaHeightInt / 1.64) Then
+            Try
+                Dim Res As Object = Nothing
+                Dim MyWeb As Object
+                MyWeb = Me.WebBrowser1.ActiveXInstance
+                MyWeb.ExecWB(Exec.OLECMDID_OPTICAL_ZOOM, execOpt.OLECMDEXECOPT_PROMPTUSER, 70, IntPtr.Zero)
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End If
+
+        If WebBrowser1.Width > Math.Round(pswaWidthInt / 1.89) And WebBrowser1.Width < Math.Round(pswaWidthInt / 1.6) Or WebBrowser1.Height > Math.Round(pswaHeightInt / 1.64) And WebBrowser1.Height < Math.Round(pswaHeightInt / 1.53) Then
+            Try
+                Dim Res As Object = Nothing
+                Dim MyWeb As Object
+                MyWeb = Me.WebBrowser1.ActiveXInstance
+                MyWeb.ExecWB(Exec.OLECMDID_OPTICAL_ZOOM, execOpt.OLECMDEXECOPT_PROMPTUSER, 80, IntPtr.Zero)
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End If
+
+        If WebBrowser1.Width > Math.Round(pswaWidthInt / 1.6) And WebBrowser1.Width < Math.Round(pswaWidthInt / 1.2) Or WebBrowser1.Height > Math.Round(pswaHeightInt / 1.53) And WebBrowser1.Height < Math.Round(pswaHeightInt / 1.16) Then
+            Try
+                Dim Res As Object = Nothing
+                Dim MyWeb As Object
+                MyWeb = Me.WebBrowser1.ActiveXInstance
+                MyWeb.ExecWB(Exec.OLECMDID_OPTICAL_ZOOM, execOpt.OLECMDEXECOPT_PROMPTUSER, 90, IntPtr.Zero)
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End If
+
+        If WebBrowser1.Width > Math.Round(pswaWidthInt / 1.2) AndAlso WebBrowser1.Height > Math.Round(pswaHeightInt / 1.16) Then
+            Try
+                Dim Res As Object = Nothing
+                Dim MyWeb As Object
+                MyWeb = Me.WebBrowser1.ActiveXInstance
+                MyWeb.ExecWB(Exec.OLECMDID_OPTICAL_ZOOM, execOpt.OLECMDEXECOPT_PROMPTUSER, 100, IntPtr.Zero)
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        End If
+    End Sub
+
+    Private Sub Form1_SizeChanged(sender As Object, e As EventArgs) Handles Me.SizeChanged
+        WebBrowser1.Height = Me.Height - 150
+    End Sub
+
+    Private Sub WebBrowser1_Navigating(sender As Object, e As WebBrowserNavigatingEventArgs) Handles WebBrowser1.Navigating
+        TextBox1.Text = "NOT READY"
+    End Sub
+
+    Private Sub InviteBt_Click(sender As Object, e As EventArgs) Handles InviteBt.Click
+        If TextBox1.Text = "READY" Then
+            Dim headElement As HtmlElement = WebBrowser1.Document.GetElementsByTagName("head")(0)
+            Dim scriptElement As HtmlElement = WebBrowser1.Document.CreateElement("script")
+            Dim element As IHTMLScriptElement = DirectCast(scriptElement.DomElement, IHTMLScriptElement)
+            element.text = "javascript:var inputs = document.getElementsByClassName('_1pu2'); 
+                            for(var i=0;i<inputs.length;i++) { inputs[i].click(); }"
+            headElement.AppendChild(scriptElement)
+            WebBrowser1.Document.InvokeScript("sayHello")
+        Else
+            MsgBox("Die Seite wurde noch nicht geladen!")
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        WebBrowser1.Document.GetElementById("u_0_11").InvokeMember("click")
+        Threading.Thread.Sleep(500)
+        SendKeys.Send("{ENTER} ")
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        login_facebook()
+    End Sub
+End Class
